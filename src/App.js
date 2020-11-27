525a17749d6c8bca7b99dfa8dfecd77b7a0cdf73
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-11-26 19:22:08 -0800

    adding a counter with useState()

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Playground</h1>
       </header>
+      <main>
+        <div className="counter">
+          <h2>Creating a counter with useState():</h2>
+          <p>You clicked {count} times</p>
+          <button onClick={() => setCount(prev => prev + 1)}>
+            Click to + 1
+          </button>
+          <button onClick={() => setCount(prev => prev - 1)}>
+            Click to - 1
+          </button>
+          <button onClick={() => setCount(0)}>
+            Reset
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
