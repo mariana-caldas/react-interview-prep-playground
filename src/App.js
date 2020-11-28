1d88af552291c778ee926cacf5652d0aebaf4944
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-11-27 16:07:56 -0800

    adding useEffect() to handle the API rendering

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { useState, UseEffect, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Counter from './components/Counter';
 import './App.css';
 import axios from 'axios';
 
 //https://randomuser.me/api
 
-//why  VS suggest to make this function async?
-
+//why VS suggests to make this function async?
 const fetchData = async () => {
   try {
     const response = await axios.get('https://randomuser.me/api');
     // handle success
     console.log(response);
-    return response;
+    return JSON.stringify(response, null, 1);
   } catch (error) {
     // handle error
     console.log(error);
-    return error;
   }
 
 }
 
 function App() {
   const [count, setCount] = useState(0);
+  const [randomUserDataJSON, setRandomUserDataJSON] = useState('');
+
+  //Why then is here and not inside fetchData function above?
+  useEffect(() => {
+    fetchData().then(response => {
+      setRandomUserDataJSON(response || 'No user found');
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -32,6 +39,9 @@ function App() {
         <Counter count={count} setCount={setCount} />
         <hr />
         <button onClick={() => { fetchData(); }}>Fetch Data</button>
+        <pre style={{ textAlign: "left" }}>
+          {randomUserDataJSON}
+        </pre>
       </main>
     </div>
   );
