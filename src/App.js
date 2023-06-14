bca1d459cd1ba4cb3dbf838c3a1c817775db876f
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2023-06-13 17:25:30 -0700

    Fix useEffect warning related to eslint array dependency

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import fetchData from './api';
-import Counter from './components/Counter';
-import User from './components/User'
-import './App.css';
+import React, { useState, useEffect } from "react";
+import fetchData from "./api";
+import Counter from "./components/Counter";
+import User from "./components/User";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [nextPageNumber, setNextPageNumber] = useState(1)
+  const [nextPageNumber, setNextPageNumber] = useState(1);
   const [userInfos, setUserInfos] = useState([]);
   // const [randomUserDataJSON, setRandomUserDataJSON] = useState();
 
   const fetchNextUser = () => {
-    fetchData(nextPageNumber).then(response => {
+    fetchData(nextPageNumber).then((response) => {
       // setRandomUserDataJSON(JSON.stringify(response));
       if (response.data === undefined) return;
-      const newUserInfos = [
-        ...userInfos,
-        ...response.data.results
-      ]
+      const newUserInfos = [...userInfos, ...response.data.results];
       setUserInfos(newUserInfos);
       setNextPageNumber(response.data.info.page + 1);
     });
-  }
+  };
 
   useEffect(() => {
-    fetchNextUser()
+    fetchNextUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const userData = userInfos.map((user, index) => {
-    return <User key={index} userFirstName={user.name.first} userLastName={user.name.last} userPicture={user.picture.thumbnail} />
-  })
+    return (
+      <User
+        key={index}
+        userFirstName={user.name.first}
+        userLastName={user.name.last}
+        userPicture={user.picture.thumbnail}
+      />
+    );
+  });
 
   return (
     <div className="App">
@@ -40,13 +45,25 @@ function App() {
         <Counter count={count} setCount={setCount} />
         <br />
         <h2>Fetching an API data with useEffect()</h2>
-        <button onClick={() => { fetchData() }}>Fetch Data</button>
+        <button
+          onClick={() => {
+            fetchData();
+          }}
+        >
+          Fetch Data
+        </button>
         {userData}
         {/* <pre style={{ textAlign: "left" }}>
           {randomUserDataJSON}
         </pre> */}
         <br />
-        <button onClick={() => { fetchNextUser() }}>Fetch Next User</button>
+        <button
+          onClick={() => {
+            fetchNextUser();
+          }}
+        >
+          Fetch Next User
+        </button>
       </main>
     </div>
   );
